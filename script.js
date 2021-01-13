5fd7e4c531a9b3c964dee7df2522a4ed3fe91b8b
RAS06 <rizwan.a.z.syed1996@gmail.com>
2021-01-13 22:00:46 +0000

    Completed Step Five. I don't know what I'm doing. Please send help.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+var buttons = document.getElementsByClassName('colorButton');
+setButtonColor(buttons[0], 0, 0, 255);
+
 var heading; 
 heading = document.getElementById('colorValue');
 heading.innerHTML = 'Hello World! I do not know what I am doing!';
+
+var red = makeColorValue();
+var green = makeColorValue();
+var blue = makeColorValue();
+
+
+function setButtonColor(button, red, green, blue) {
+  button.setAttribute('style',
+'background-color: rgb('+ 255 +','+ 255 +','+ 0 +');'
+);
+}
+
+function makeColorValue () {
+  return Math.round(Math.random() * 255);
+}
