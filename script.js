523a97ba9da8647fadfc9638d9608998f5f04149
RAS06 <rizwan.a.z.syed1996@gmail.com>
2021-01-15 00:58:45 +0000

    Finished off step six. Did a little bit of seven. Color display system in the heading system, moving data from HTML to JS back to HTML.

--- script.js
+++ script.js
@@ -3,19 +3,35 @@ setButtonColor(buttons[0], 0, 0, 255);
 
 var heading; 
 heading = document.getElementById('colorValue');
-heading.innerHTML = 'Hello World! I do not know what I am doing!';
+
+
+var answerButton = Math.round(Math.random() * (buttons.length - 1));
+
+
+var answerMessage = document.getElementById('answer');
+
+for (var i = 0; i < buttons.length; i++) {
 
 var red = makeColorValue();
 var green = makeColorValue();
 var blue = makeColorValue();
 
+  setButtonColor(buttons[i], red, green, blue);
+
+
+  if (i === answerButton) {
+    heading.innerHTML = `(${red}, ${green}, ${blue})`;
+  }
+}
+
 
 function setButtonColor(button, red, green, blue) {
   button.setAttribute('style',
-'background-color: rgb('+ 255 +','+ 255 +','+ 0 +');'
+'background-color: rgb('+ red +','+ green +','+ blue +');'
 );
 }
 
+
 function makeColorValue () {
-  return Math.round(Math.random() * 255);
+  return Math.round(Math.random()*255);
 }
