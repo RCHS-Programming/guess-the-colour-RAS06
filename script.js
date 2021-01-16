089d00c5ba5a2b398f918cf8cf88362bea5a66ae
RAS06 <rizwan.a.z.syed1996@gmail.com>
2021-01-16 18:57:45 +0000

    Sat around and choked violently on the challenge in the ninth step. Send help. SOS. *Confused Screaming.*

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ setButtonColor(buttons[0], 0, 0, 255);
 var heading; 
 heading = document.getElementById('colorValue');
 
+var answerMessage = document.getElementById('answer');
+
+var background = document.getElementsByTagName('body');
+
+
+function startGame () {
+
+  answerMessage.innerHTML = "";
 
 var answerButton = Math.round(Math.random() * (buttons.length - 1));
 
 
-var answerMessage = document.getElementById('answer');
 
 for (var i = 0; i < buttons.length; i++) {
 
@@ -21,6 +28,16 @@ var blue = makeColorValue();
 
   if (i === answerButton) {
     heading.innerHTML = `(${red}, ${green}, ${blue})`;
+  }
+
+    buttons[i].addEventListener('click', function() {
+      if (this === buttons[answerButton]) {
+        answerMessage.innerHTML = "Correct!";
+      } else {
+        answerMessage.innerHTML = "Wrong Answer! Guess  Again!";
+
+      }
+    });
   }
 }
 
@@ -35,3 +52,12 @@ function setButtonColor(button, red, green, blue) {
 function makeColorValue () {
   return Math.round(Math.random()*255);
 }
+
+function setBackgroundColor(background) {
+  
+}
+
+startGame();
+
+
+document.getElementById('resetButton').addEventListener('click', startGame);
